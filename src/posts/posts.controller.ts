882843dc6434d6019b69fed041a6830b8b6d2777
onlyhy <hyuestu@163.com>
2020-04-17 17:06:03 +0800

    5、Put、Delete请求和URL路径参数

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,21 +1,22 @@
-import { Controller, Get, Post, Body, Query, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query, Param, Put, Delete } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiProperty } from '@nestjs/swagger';
 
 // 类：数据传输对象
 // Dto:DataTransferObject
 class CreatePostDto {
-    @ApiProperty({description:'帖子标题'})
+    @ApiProperty({ description: '帖子标题' })
     title: string
     @ApiProperty({ description: '帖子内容' })
     content: string
 }
 
 
+
 @Controller('posts')
 @ApiTags('帖子')
 export class PostsController {
     @Get()
-    @ApiOperation({ summary: '显示博客列表' })
+    @ApiOperation({ summary: '显示帖子列表' })
     index() {
         return []
     }
@@ -29,10 +30,28 @@ export class PostsController {
     }
 
     @Get(':id')
-    detail() {
+    @ApiOperation({ summary: '帖子详情' })
+    detail(@Param('id') id: string) {
         return {
-            id: 1,
+            id: id,
             title: '123'
         }
     }
+
+    @Put(':id')
+    @ApiOperation({ summary: '编辑帖子' })
+    update(@Param('id') id: string, @Body() body: CreatePostDto) {
+        return {
+            success: true
+        }
+    }
+
+    @Delete(':id')
+    @ApiOperation({ summary: '删除帖子' })
+    remove(@Param('id') id: string){
+        return {
+            success: true
+        }
+    }
+
 }
